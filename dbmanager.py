db05363c5313c2211d6859f0a290d11a7b04a1e4
xuweiyun <xwy314dq@gmail.com>
2017-09-13 15:23:57 +0800

    conf

--- dbmanager.py
+++ dbmanager.py
@@ -16,7 +16,7 @@ class CrawlDatabaseManager:
     def __init__(self, max_num_thread):
         # connect mysql server
         try:
-            cnx = mysql.connector.connect(host=self.SERVER_IP, user='root', password='jcplanb.com')
+            cnx = mysql.connector.connect(host=self.SERVER_IP, user='root', password='jctest')
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 print "Something is wrong with your user name or password"
@@ -47,7 +47,7 @@ class CrawlDatabaseManager:
         dbconfig = {
             "database": self.DB_NAME,
             "user": "root",
-            "password": "jcplanb.com",
+            "password": "jctest",
             "host": self.SERVER_IP,
         }
         self.cnxpool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool",
@@ -242,6 +242,112 @@ class CrawlDatabaseManager:
             con.commit()
         except mysql.connector.Error as err:
             # print 'finishUrl() ' + err.msg
+            return
+        finally:
+            cursor.close()
+            con.close()
+
+    ##########
+    ## extract_url
+    # initial extract_url
+    def init_extract_url(self):
+        con = self.cnxpool.get_connection()
+        cursor = con.cursor()
+        try:
+            if time.strftime("%H%M%S", time.localtime()) < '013000':
+                user_init = 'update crawl_url SET status_extract="new" where status_extract = "downloading"'
+            else:
+                user_init = None
+
+            if user_init <> None:
+                cursor.execute(user_init)
+                con.commit()
+        except mysql.connector.Error as err:
+            print 'init_extract_url() ' + err.msg
+            return
+        finally:
+            cursor.close()
+            con.close()
+
+    # get an web catorgy from queue
+    def dequeue_extract_url(self):
+        con = self.cnxpool.get_connection()
+        cursor = con.cursor(dictionary=True)
+        try:
+            # use select * for update to lock the rows for read
+            query = (
+                "SELECT `id`, `url_md5`, `url`, `catorgy_url` FROM crawl_url WHERE status_extract='new' ORDER BY `id` ASC LIMIT 1 FOR UPDATE")
+            cursor.execute(query)
+            if cursor.rowcount is 0:
+                return None
+            row = cursor.fetchone()
+            if row is None:
+                return None
+            update_query = ("UPDATE crawl_url SET `status_extract`='downloading' WHERE `id`=%d") % (row['id'])
+            cursor.execute(update_query)
+            con.commit()
+            return row
+        except mysql.connector.Error as err:
+            print 'dequeue_extract_url() ' + err.msg
+            return None
+        finally:
+            cursor.close()
+            con.close()
+
+    def finish_extract_url(self, index):
+        con = self.cnxpool.get_connection()
+        cursor = con.cursor()
+        try:
+            update_query = ("UPDATE crawl_url SET `status_extract`='done' WHERE `id`=%d") % (index)
+            cursor.execute(update_query)
+            con.commit()
+        except mysql.connector.Error as err:
+            # print 'finishUrl() ' + err.msg
+            return
+        finally:
+            cursor.close()
+            con.close()
+
+    # enqueue url insert table
+    def enqueue_crawl_url(self, mid, kw, **kwargs):
+        print('------')
+        con = self.cnxpool.get_connection()
+        cursor = con.cursor(buffered=True)
+        try:
+            keys = 'url_md5'
+            values = (mid,)
+            values_stmt = '%s'
+
+            for key in kwargs:
+                keys += ', ' + key
+                values_stmt += ', %s'
+                values += (kwargs[key],)
+            add_doc_info = ("INSERT INTO crawl_url (%s) VALUES ") % (keys)
+            add_doc_info += '(' + values_stmt + ')'
+
+            update_kw = u'UPDATE crawl_url SET kwords = IF(kwords is NULL, "%s", IF(kwords like "%%%s%%", kwords, CONCAT(kwords, ",%s"))) where url_md5 = "%s"' % (
+                kw, kw, kw, mid)
+            print(update_kw)
+            query = ('SELECT `id` FROM crawl_url WHERE url_md5="%s"') % (mid)
+            print(query)
+
+            cursor.execute(query)
+            print(query)
+            print(cursor.rowcount)
+            if cursor.rowcount < 1:
+                print(add_doc_info)
+                print(values)
+                cursor.execute(add_doc_info, values)
+            else:
+                cursor.execute(update_kw)
+                print(update_kw)
+
+            # cursor.execute(add_doc_info, values)
+            # cursor.execute(update_kw)
+            # commit this transaction, please refer to "mysql transaction" for more info
+            con.commit()
+        except mysql.connector.Error as err:
+            print 'insert_weibo() ' + err.msg
             return
         finally:
             cursor.close()
